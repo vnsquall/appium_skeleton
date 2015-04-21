f61addb760acb4ed9645530e3338e65968519c4b
Khanh Nguyen <vnsquall@gmail.com>
2015-04-21 16:36:35 +0700

    Initial commit

---
+++ src/test/java/util/Constant.java
@@ -0,0 +1,29 @@
+package util;
+
+/**
+ * Created by khanh.nguyen on 4/21/2015.
+ */
+public final class Constant {
+    public static final String SERVER_ADDRESS = "http://127.0.0.1:4723/wd/hub";
+    public static final String APP_PACKAGE_LIVE = "android package";
+    public static final String APP_PACKAGE_STAGING = "android package";
+
+    public static final String LOCAL_APP_LIVE = "apk file name";
+    public static final String LOCAL_APP_STAGING = "apk file name";
+
+    public static final String WEBVIEW_LIVE= "WEBVIEW_name";
+    public static final String WEBVIEW_STAGING = "WEBVIEW_name";
+
+    public static final String AUTOMATION_NAME = "Appium";
+    public static final String BROWSER_NAME = "";
+    public static final String APPIUM_VERSION = "1.2.3";
+    public static final String PLATFORM_NAME = "Android";
+    public static final String PLATFORM_VERSION = "4.4";
+    public static final String DEVICE_NAME = "Android Emulator";
+    public static final String APP_ACTIVITY = ""; //main activity
+    public static final String SCREENSHOT_PATH = "Path/here";
+    public static final String USERNAME = "user@mail.com";
+    public static final String PASSWORD = "a12345";
+
+    public static final int TIMEOUT = 30;
+}
